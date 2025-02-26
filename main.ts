ec2ad143f53f303814ce49306a7825b1184df1a9
Joel Duscha <jfduscha@gmail.com>
2025-02-26 17:09:32 +0100

    make it search for a fixed heading

--- main.ts
+++ main.ts
@@ -19,25 +19,70 @@ export default class MyPlugin extends Plugin {
 		  
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		const ribbonIconEl = this.addRibbonIcon('pdf-file', 'Summarize current file', async (evt: MouseEvent) => {
+			await handleSummarizeFile(this.app);
 		});
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		statusBarItemEl.setText('Found a diary');
 
 
 		this.addCommand({
-			id: "joel und tim",
-			name: "joel und tim",
-			callback: () => {
-			  new Notice("Hello, Obsidian!");
+			id: "summarize-file",
+			name: "Summarize File",
+			callback: async () => {
+				await handleSummarizeFile(this.app);
 			}
-		  });
+		});
+		
+		async function handleSummarizeFile(app: App) {
+			const file = app.workspace.getActiveFile();
+			if (!file) {
+				new Notice("No active file found!");
+				return;
+			}
+		
+			try {
+				const content = await app.vault.read(file);
+				const documentationContent = summarizeFile(content);
+		
+				if (documentationContent.length > 0) {
+					new Notice(documentationContent);
+				} else {
+					new Notice("No Dokumentation section found in this file.");
+				}
+			} catch (err) {
+				new Notice("Error reading file: " + err.message);
+				console.error(err);
+			}
+		}
+		
+		function summarizeFile(content: string) {
+			const lines = content.split('\n');
+			let inSection = false;
+			let sectionContent = [];
+		
+			for (const line of lines) {
+				if (line.startsWith('# Dokumentation')) {
+					inSection = true;
+					continue;
+				}
+		
+				if (inSection) {
+					if (line.startsWith('# ')) break;
+					sectionContent.push(line.trim());
+				}
+			}
+			
+			return sectionContent.join('\n');
+		}
+		
+		  		
+				
+		/*
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
@@ -48,6 +93,7 @@ export default class MyPlugin extends Plugin {
 			}
 		});
 		// This adds an editor command that can perform some operation on the current editor instance
+		
 		this.addCommand({
 			id: 'sample-editor-command',
 			name: 'Sample editor command',
@@ -56,6 +102,7 @@ export default class MyPlugin extends Plugin {
 				editor.replaceSelection('Sample Editor Command');
 			}
 		});
+		
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
 		this.addCommand({
 			id: 'open-sample-modal-complex',
@@ -75,6 +122,8 @@ export default class MyPlugin extends Plugin {
 				}
 			}
 		});
+
+		*/
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
